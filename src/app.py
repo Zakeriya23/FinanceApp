0b10b95cdb47468103c156463e1525936b875960
Zakeriya <muhumed@pdx.edu>
2023-08-30 16:19:30 -0700

    CODE FINISHED

--- src/app.py
+++ src/app.py
@@ -1,7 +1,6 @@
 #******************************************************************************
 # Author:           Zakeriya Muhumed
 # Date:             Aug 20, 2023
-# Sources:          https://www.youtube.com/watch?v=teBfyozQj4w
 #******************************************************************************/
 
 from auth.main_ui import main
